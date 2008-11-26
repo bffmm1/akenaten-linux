53a4c5022f41e749aae1f0d2b3d5ca9a48f24235
anoochit <anoochit@e10c0ba4-a64b-11dd-9a14-c1645b63b0c6>
2008-11-26 06:51:40 +0000

    update to default splash

--- trunk/gen-ubuntu-goo-apt-downlist.sh
+++ trunk/gen-ubuntu-goo-apt-downlist.sh
@@ -12,7 +12,7 @@ apt-get update
 locale-gen en_US.UTF-8
 echo "apt standard package"
 apt-get install ubuntu-standard casper locales console-data ubiquity
-apt-get install discover1 laptop-detect os-prober rcconf
+apt-get install discover laptop-detect os-prober rcconf
 apt-get install $xpack
 apt-get install linux-generic
 echo "clean"
